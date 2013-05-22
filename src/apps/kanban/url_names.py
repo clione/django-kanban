575c813574fb4ca44f374ea9ab14af0853501cb8
Oscar Carballal Prego <info@oscarcp.com>
2013-05-22 15:00:31 +0200

    Added some names to kanban url names

--- src/apps/kanban/url_names.py
+++ src/apps/kanban/url_names.py
@@ -1,4 +1,14 @@
 # Copyright 2013 Clione Software
 # Licensed under MIT license. See LICENSE for details.
 
-#url_names
+ADD_TASK
+
+EDIT_TASK
+
+DELETE_TASK
+
+ADD_BOARD
+
+EDIT_BOARD
+
+DELETE_BOARD
