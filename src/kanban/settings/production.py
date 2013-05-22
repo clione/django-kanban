0eb0f759717b8a7b38d8121f15c655845c549540
Oscar Carballal Prego <info@oscarcp.com>
2013-05-22 14:41:37 +0200

    Added settings for kanban

---
+++ src/kanban/settings/production.py
@@ -0,0 +1,52 @@
+# Copyright 2013 Clione Software
+# Licensed under MIT license. See LICENSE for details.
+
+from kanban.settings.defaults import *
+
+# Registration mail settings. Please use a different mail server and account
+# during development than in production.
+# EMAIL_HOST = ""
+# EMAIL_PORT= 25
+# EMAIL_HOST_USER= ""
+# EMAIL_HOST_PASSWORD= ""
+# DEFAULT_FROM_EMAIL = ""
+# EMAIL_USE_TLS = True
+
+# Time and zone configuration
+TIME_ZONE = 'Europe/London'
+LANGUAGE_CODE = 'en-gb'
+
+# Cache backend. Default: local memory storage
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake'
+    }
+}
+
+# Who will we alert?
+ADMINS = (
+    ('YourAdmin', 'user@host.com'),
+)
+MANAGERS = ADMINS
+
+# Database configuration. Default: sqlite3
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+SECRET_KEY = ''
+
+FIXTURE_DIRS = (
+    (cwd + '/e_cidadania/fixtures/'),
+)
+
+# Not needed with debug, but just in case we set it to all.
+ALLOWED_HOSTS = ['']
