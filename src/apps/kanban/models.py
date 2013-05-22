3c0ad0507aa340eb6be3a8a272efb34d5de983cf
Oscar Carballal Prego <info@oscarcp.com>
2013-05-22 13:48:03 +0200

    Added models and fields. also added some minor validation and permissions

--- src/apps/kanban/models.py
+++ src/apps/kanban/models.py
@@ -4,32 +4,75 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
 
 
 class Board(models.Model):
 
-	"""
-	Board data model, the board contains all the rows and the notes of the
-	Kanban.
-	"""
-	name = models.CharField(_('Name'), max_length=200)
-	created = models.DateTimeField(_('Date of creation'), auto_now_add=True)
+    """
+    Board data model, the board contains all the rows and the notes of the
+    Kanban.
 
-	class Meta:
+    .. versionadded:: 0.1
+    """
+    name = models.CharField(_('Name'), max_length=255)
+    pub_date = models.DateTimeField(_('Created'), auto_now_add=True)
 
-	def __unicode__(self):
-		return self.name
+    class Meta:
+        permissions = (
+            ('view_board', 'Can view the board'),
+        )
+        ordering = ['name']
+        verbose_name = _('Board')
+        verbose_name_plural = _('Boards')
+        get_latest_by = 'pub_date'
+
+    def __unicode__(self):
+        return self.name
 
 
 class Column(models.Model):
 
-	"""
-	"""
-	title = models.CharField(_('Title'), max_length=200)
+    """
+    Column data model. This will contain the tasks, and there can be multiple
+    columns on the board.
+
+    .. versionadded:: 0.1
+    """
+    board = models.ForeignKey(Board)
+    title = models.CharField(_('Title'), max_length=255)
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = _('Column')
+        verbose_name_plural = _('Columns')
+
+    def __unicode__(self):
+        return self.title
 
 
-class Note(models.Model):
+class Task(models.Model):
 
-	"""
-	"""
-	pass
+    """
+    Task data model. This will store all the information about the tasks.
+
+    .. versionadded:: 0.1
+    """
+    column = models.ForeignKey(Column)
+    text = models.CharField(_('Name'), max_length=255)
+    responsible = models.CharField(_('Responsible'), max_length=255)
+    progress = models.PositiveIntegerField(_('Progress'))
+    pub_date = models.CharField(_('Created'), auto_now_add=True)
+
+    class Meta:
+        ordering = ['pub_date']
+        verbose_name = _('Task')
+        verbose_name_plural = _('Tasks')
+        get_latest_by = 'pub_date'
+
+    def __unicode__(self):
+        return self.text
+
+    def clean(self):
+        if self.progress > 100:
+            raise ValidationError(_('A task cannot be completed beyond 100%.'))
