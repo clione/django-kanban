7e396ad548dd733a3a54a86c97e8211480162037
Oscar Carballal Prego <info@oscarcp.com>
2013-05-22 12:49:36 +0200

    Added some file placeholders and created first sketch of data models

--- src/apps/kanban/models.py
+++ src/apps/kanban/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+# Copyright 2013 Clione Software
+# Licensed under MIT license. See LICENSE for details.
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
+
+class Board(models.Model):
+
+	"""
+	Board data model, the board contains all the rows and the notes of the
+	Kanban.
+	"""
+	name = models.CharField(_('Name'), max_length=200)
+	created = models.DateTimeField(_('Date of creation'), auto_now_add=True)
+
+	class Meta:
+
+	def __unicode__(self):
+		return self.name
+
+
+class Column(models.Model):
+
+	"""
+	"""
+	title = models.CharField(_('Title'), max_length=200)
+
+
+class Note(models.Model):
+
+	"""
+	"""
+	pass
