8b530fadea949b144b696f792391fee1eb1e551d
Oscar Carballal Prego <info@oscarcp.com>
2013-05-22 15:12:48 +0200

    Added the home view

--- src/apps/kanban/views.py
+++ src/apps/kanban/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+# Copyright 2013 Clione Software
+# Licensed under MIT license. See LICENSE for details.
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseNotFound, \
+                        HttpResponseBadRequest, HttpResponseServerError
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.views.generic.detail import DetailView
+from django.template import RequestContext
+
+from guardian.shortcuts import assign_perm, get_users_with_perms, remove_perm, \
+                               get_perms
+from guardian.core import ObjectPermissionChecker
